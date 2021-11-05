67a774c7b36b51a1750e7f1b75004ab9e1d32815
HiroshiOyamada <h5y1m141+github@gmail.com>
2021-11-05 14:38:51 +0900

    管理画面で注文内容を確認できるように修正

--- app/dashboards/order_item_dashboard.rb
+++ app/dashboards/order_item_dashboard.rb
@@ -1,6 +1,6 @@
-require 'administrate/base_dashboard'
+require "administrate/base_dashboard"
 
-class CustomerDashboard < Administrate::BaseDashboard
+class OrderItemDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -8,11 +8,14 @@ class CustomerDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    order: Field::BelongsTo,
     id: Field::Number,
-    uid: Field::String,
+    product_name: Field::String,
+    product_unit_price: Field::Number,
+    quantity: Field::Number,
+    sub_total: Field::Number,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    stripe_customer_id: Field::String
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -21,28 +24,34 @@ class CustomerDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    order
     id
-    uid
-    created_at
+    product_name
+    product_unit_price
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    order
     id
-    uid
+    product_name
+    product_unit_price
+    quantity
+    sub_total
     created_at
     updated_at
-    stripe_customer_id
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    customer_payment_methods
-    uid
-    stripe_customer_id
+    order
+    product_name
+    product_unit_price
+    quantity
+    sub_total
   ].freeze
 
   # COLLECTION_FILTERS
@@ -57,10 +66,10 @@ class CustomerDashboard < Administrate::BaseDashboard
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how customers are displayed
+  # Overwrite this method to customize how order items are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(customer)
-  #   "Customer ##{customer.id}"
+  # def display_resource(order_item)
+  #   "OrderItem ##{order_item.id}"
   # end
 end
