146dad2406b30f6b40d42481d9beb3dde0b822c8
HiroshiOyamada <h5y1m141+github@gmail.com>
2021-11-08 13:16:54 +0900

    adminログインの初期化時に必要になるFirebaseのKey情報をERBのテンプレートを通じて渡す形式に変更

--- app/controllers/admin/firebase_sessions_controller.rb
+++ app/controllers/admin/firebase_sessions_controller.rb
@@ -7,6 +7,16 @@ module Admin
 
     def new
       redirect_to users_path if logged_in?
+      @config = {
+        api_key: ENV['FIREBASE_API_KEY'],
+        auth_domain: ENV['FIREBASE_AUTH_DOMAIN'],
+        database_url: ENV['FIREBASE_DATABASE_URL'],
+        project_id: ENV['FIREBASE_PROJECT_ID'],
+        storage_bucket: ENV['FIREBASE_STORAGE_BUCKET'],
+        messaging_sender_id: ENV['FIREBASE_MESSAGING_SENDER_ID'],
+        app_id: ENV['FIREBASE_APP_ID'],
+        measurement_id: ENV['FIREBASE_MEASUREMENT_ID']
+      }
     end
 
     def create
