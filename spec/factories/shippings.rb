4ca00fa15d1463f5da182dff8edffaccf6bb4049
HiroshiOyamada <h5y1m141+github@gmail.com>
2021-11-09 13:52:32 +0900

    request_listに関するテストケースを作成

--- spec/factories/shippings.rb
+++ spec/factories/shippings.rb
@@ -2,5 +2,17 @@ FactoryBot.define do
   factory :shipping do
     order { nil }
     status { 1 }
+
+    trait :shipping_request do
+      status { 1 }
+    end
+
+    trait :shipping_in_ready do
+      status { 2 }
+    end
+
+    trait :shipping_complete do
+      status { 3 }
+    end
   end
 end
--- spec/models/shipping_spec.rb
+++ spec/models/shipping_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Shipping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.request_list' do
+    subject { described_class.request_list.count }
+
+    context '注文受け付けだけが存在する場合' do
+      before do
+        order = create(:order, total_price: 1000)
+        create(:shipping, :shipping_request, order: order)
+      end
+
+      it '抽出結果は1件になる' do
+        expect(subject).to eq 1
+      end
+    end
+
+    context '注文受け付けが2件、受注対応完了が1件存在する場合' do
+      before do
+        order1 = create(:order, total_price: 10_000)
+        order2 = create(:order, total_price: 20_000)
+        create(:shipping, :shipping_request, order: order1)
+        create(:shipping, :shipping_in_ready, order: order1)
+        create(:shipping, :shipping_request, order: order2)
+      end
+
+      it '抽出結果は1件になる' do
+        expect(subject).to eq 1
+      end
+    end
+  end
 end
