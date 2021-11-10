284f577f42079aaca6ab79b97c3903fb710197c6
HiroshiOyamada <h5y1m141+github@gmail.com>
2021-11-10 13:33:27 +0900

    Failするrequest spec修正

--- spec/requests/admin/complete_shippings_spec.rb
+++ spec/requests/admin/complete_shippings_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Admin::CompleteShippings", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admin/complete_shippings/index"
+RSpec.describe 'Admin::CompleteShippings', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/admin/complete_shippings'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
